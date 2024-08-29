fd44f72a39436841566a195dce5e61198bee2779
RAMEES-KKVL <rameesp41750@gmail.com>
2024-08-29 19:09:24 +0530

    Completed UI design of recent companies component

--- src/app/portfolio/portfolio.module.ts
+++ src/app/portfolio/portfolio.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from "@angular/core";
 import { MainPagePortfolio } from "./components/mainPage/mainpage.component";
-import { HeaderComponent } from "../home/components/header/header.component";
 import { HomeModule } from "../home/home.module";
 import { RecentWorksComponent } from "./components/recentWorks/recentworks.component";
+import { WorkedCompaniesComponent } from "./components/workedCompanies/workedCompanies.component";
 
 @NgModule({
     declarations: [
         MainPagePortfolio,
-        RecentWorksComponent
+        RecentWorksComponent,
+        WorkedCompaniesComponent
     ],
     imports: [
         HomeModule
